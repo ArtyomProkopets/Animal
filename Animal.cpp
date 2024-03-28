ff1126bca75402f70ed6d89bbed06e4722eb5603
ArtyomProkopets <artyomprokopets@gmail.com>
2024-03-28 20:51:05 +0300

    fix

--- Animal.cpp
+++ Animal.cpp
@@ -4,17 +4,13 @@
 Animal::Animal(): mass(0), sex(NULL), color(""), age(0){
 }
 
-
-Animal::Animal(float a_mass, char *a_sex, char *a_color, int a_age) {
-  mass = a_mass;
-
+Animal::Animal(float a_mass, const char* a_sex, const char* a_color, int a_age)
+    : mass(a_mass), sex(nullptr), color(nullptr), age(a_age) {
   sex = new char[strlen(a_sex) + 1];
   strcpy(sex, a_sex);
 
   color = new char[strlen(a_color) + 1];
   strcpy(color, a_color);
-
-  age = a_age;
 }
 
 Animal::Animal(const Animal& copy) {
@@ -82,6 +78,34 @@ void Animal::SetColor(const char *newColor) {
   strcpy(color, newColor);
 }
 
+Animal &Animal::operator=(const Animal &other) {
+  if (this == &other) {
+    return *this;
+  } else {
+    mass = other.mass;
+    age = other.age;
+    SetColor(other.color);
+    SetSex(other.sex);
+    return *this;
+  }
+}
+
+Animal &Animal::operator=(Animal &&other){
+  if (this == &other) {
+    return *this;
+  } else {
+    mass = move(other.mass);
+    age = move(other.age);
+    color = move(other.color);
+    sex = move(other.sex);
+    other.mass = 0.0;
+    other.age = 0;
+    other.color = nullptr;
+    other.sex = nullptr;
+    return *this;
+  }
+}
+
 void Dog::WhatDoesSay(){
   std::cout << "Woof! Woof!" << std::endl;
 }
@@ -94,7 +118,7 @@ Dog::Dog(char *d_name, Breed d_race, float d_mass, char* d_sex, char* d_color, i
   name = new char[strlen(d_name) + 1];
   strcpy(name, d_name);
 
-  race(d_race);
+  race = d_race;
 }
 
 Dog::Dog(const Dog &copy): Animal(copy), race(copy.race) {
@@ -112,6 +136,7 @@ Dog::~Dog(){
 
 Dog &Dog::operator=(Dog &copy) {
   if (this != &copy){
+    Animal::operator=(copy);
     delete[] name;
     name = new char[strlen(copy.name) + 1];
     strcpy(name, copy.name);
@@ -120,5 +145,136 @@ Dog &Dog::operator=(Dog &copy) {
   return *this;
 }
 
+Dog &Dog::operator=(Dog &&move) {
+  if (this != &move) {
+    Animal::operator=(std::move(move));
+    delete[] name;
+    name = move.name;
+    move.name = nullptr;
+    race = move.race;
+  }
+  return *this;
+}
 
+char *Dog::GetName() {
+  return name;
+}
 
+Breed Dog::GetRace() {
+  return race;
+}
+
+void Dog::SetName(char *newName) {
+  name = newName;
+}
+
+void Dog::SetRace(Breed newRace) {
+  race = newRace;
+}
+
+void Fox::WhatDoesSay() {
+  switch (type) {
+    case TypeFox::ArcticFox:
+      std::cout << "Wa-pa-pa-pa-pa-pa-pow!\n"
+                   "Wa-pa-pa-pa-pa-pa-pow!\n"
+                   "Wa-pa-pa-pa-pa-pa-pow!" << std::endl;
+      break;
+    case TypeFox::RedFox:
+      std::cout << "Hatee-hatee-hatee-ho!\n"
+                   "Hatee-hatee-hatee-ho!\n"
+                   "Hatee-hatee-hatee-ho!" << std::endl;
+      break;
+    case TypeFox::FennecFox:
+      std::cout << "Ring-ding-ding-ding-dingeringeding!\n"
+                   "Gering-ding-ding-ding-dingeringeding!\n"
+                   "Gering-ding-ding-ding-dingeringeding!" << std::endl;
+      break;
+    case TypeFox::GrayFox:
+      std::cout << "Joff-tchoff-tchoffo-tchoffo-tchoff!\n"
+                   "Tchoff-tchoff-tchoffo-tchoffo-tchoff!\n"
+                   "Joff-tchoff-tchoffo-tchoffo-tchoff!" << std::endl;
+      break;
+    case TypeFox::SwiftFox:
+      std::cout << "Jacha-chacha-chacha-chow!\n"
+                   "Chacha-chacha-chacha-chow!\n"
+                   "Chacha-chacha-chacha-chow!" << std::endl;
+      break;
+  }
+}
+
+Fox::Fox() : name(nullptr), type(TypeFox::ArcticFox), numberRabbitsEaten(0) {}
+
+Fox::Fox(const char* name, TypeFox type, float mass, const char* sex, int age, const char* color)
+    : Animal(mass, sex, color, age), name(nullptr), type(type), numberRabbitsEaten(0) {
+  this->name = new char[strlen(name) + 1];
+  strcpy(this->name, name);
+}
+
+Fox::Fox(const Fox& other) : Animal(other), type(other.type), numberRabbitsEaten(other.numberRabbitsEaten) {
+  this->name = new char[strlen(other.name) + 1];
+  strcpy(this->name, other.name);
+}
+
+Fox::Fox(Fox&& other): Animal(std::move(other)), name(other.name), type(other.type), numberRabbitsEaten(other.numberRabbitsEaten) {
+  other.name = nullptr;
+  other.numberRabbitsEaten = 0;
+}
+
+Fox::~Fox() {
+  delete[] name;
+}
+
+Fox& Fox::operator=(const Fox& other) {
+  if (this != &other) {
+    Animal::operator=(other);
+    delete[] name;
+    name = new char[strlen(other.name) + 1];
+    strcpy(name, other.name);
+    type = other.type;
+    numberRabbitsEaten = other.numberRabbitsEaten;
+  }
+  return *this;
+}
+
+Fox& Fox::operator=(Fox&& other){
+  if (this != &other) {
+    Animal::operator=(std::move(other));
+    delete[] name;
+    name = other.name;
+    other.name = nullptr;
+    type = other.type;
+    numberRabbitsEaten = other.numberRabbitsEaten;
+    other.numberRabbitsEaten = 0;
+  }
+  return *this;
+}
+
+const char* Fox::GetName() const {
+  return name;
+}
+
+TypeFox Fox::GetType() const {
+  return type;
+}
+
+int Fox::GetNumberRabbitsEaten() const {
+  return numberRabbitsEaten;
+}
+
+void Fox::SetName(const char* newName) {
+  delete[] name;
+  name = new char[strlen(newName) + 1];
+  strcpy(name, newName);
+}
+
+void Fox::SetType(TypeFox newType) {
+  type = newType;
+}
+
+void Fox::SetNumberRabbitsEaten(int newNumber) {
+  numberRabbitsEaten = newNumber;
+}
+
+void Fox::Eat() {
+  numberRabbitsEaten++;
+}
--- Animal.h
+++ Animal.h
@@ -12,9 +12,11 @@ class Animal{
 
  public:
   Animal();
-  Animal(float a_mass, char *a_sex, char *a_color, int a_age);
+  Animal(float a_mass, const char* a_sex, const char* a_color, int a_age);
   Animal(const Animal& copy);
   Animal(Animal&& move);
+  Animal &operator=(const Animal &other);
+  Animal &operator=(Animal &&other);
   virtual ~Animal();
   float GetMass();
   char* GetSex();
@@ -42,6 +44,7 @@ enum class Breed {
 
 class Dog: public Animal{
  private:
+  Dog(char *d_name, Breed d_race, float d_mass, char *d_sex, char *d_color, int d_age);
   char* name;
   Breed race;
  public:
@@ -53,29 +56,39 @@ class Dog: public Animal{
   ~Dog();
   Dog& operator=(Dog &copy);
   Dog& operator=(Dog &&move);
-  float GetName();
-  char* GetRace();
-  void SetName();
-  void SetRace();
+  char* GetName();
+  Breed GetRace();
+  void SetName(char* newName);
+  void SetRace(Breed newRace);
+};
+
+enum class TypeFox {
+  ArcticFox,
+  RedFox,
+  FennecFox,
+  GrayFox,
+  SwiftFox
 };
 
 class Fox: public Animal{
  private:
   char* name;
-  char* type;
+  TypeFox type;
   int numberRabbitsEaten;
  public:
   void WhatDoesSay() override;
-  Fox(char* name, char* type);
+  Fox();  // Конструктор по умолчанию
+  Fox(const char* name, TypeFox type, float mass, const char* sex, int age, const char* color);
+  Fox(const Fox& other);  // Конструктор копирования
+  Fox(Fox&& other);  // Конструктор перемещения
   ~Fox();
-  Fox& operator=(char &other);
-  Fox& operator=(char &&other);
-  char* GetName();
-  char* GetType();
-  int GetNumberRabbitsEaten();
-  void SetName();
-  void SetType();
-  void SetNumberRabbitsEaten();
+  Fox& operator=(const Fox& other);
+  Fox& operator=(Fox&& other);
+  const char* GetName() const;
+  TypeFox GetType() const;
+  int GetNumberRabbitsEaten() const;
+  void SetName(const char* newName);
+  void SetType(TypeFox newType);
+  void SetNumberRabbitsEaten(int newNumber);
   void Eat();
-
 };
